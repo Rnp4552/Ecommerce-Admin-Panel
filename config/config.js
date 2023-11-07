8a6a620830a3c78a2c8a85894782f413b22ae681
Rnp4552 <100115956+Rnp4552@users.noreply.github.com>
2023-11-07 14:35:15 +0530

    Delete config directory

--- config/config.js
+++
@@ -1,6 +0,0 @@
-port = 5500
-SESSSION_SECRET_KEY = "SessionSecretKey"
-clientID = "287086687546-2p7fksdnf19ap5buifndp7cedooj4shk.apps.googleusercontent.com"
-clientSecret =  "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"
-callbackURL = "http://localhost:2929/auth/google/user"
-MONGODB_URL = "mongodb+srv://ridham2234:ridhamscluster@mycluster.tblzfpe.mongodb.net/?retryWrites=true&w=majority"
